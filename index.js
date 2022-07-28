489e15c2b65fa08fa86e0ce692579b915128e67d
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-07-28 14:34:07 -0700

    solved grade function 7

--- index.js
+++ index.js
@@ -283,11 +283,21 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(score){
+  if(score >= 90) {
+    return "you got an A";
+  } else if(score >= 80 && score < 90) {
+    return "you got a B";
+  } else if(score >= 70 && score < 80) {
+    return "you got a C";
+  } else if(score >=60 && score < 70) {
+    return "you got a D";
+  } else {
+    return "you got an F";
+  }
 }
 
-
+console.log(grade(68));
 
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
 
